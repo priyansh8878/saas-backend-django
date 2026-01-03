5d4f5c73813d7b4bc476f75bc368a543a43359e7
Priyansh <priyanshchoudhary93022@gmail.com>
2026-01-03 21:48:43 +0530

    Prepare project for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-669hi4ng=&^+r+8xm$0is2^r6c4wrnfm)$sgqu(0z7wz#ckro7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
